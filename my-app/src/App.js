f47323bf0d86d6e9c48935a955d265cf6a207183
KajalNikam <kajalnikam29@gmail.com>
2023-09-22 05:39:48 -0400

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TableLayout from './Table/TableLayout';
+
+function App() {
+  return (
+    <div className="App">
+      <TableLayout />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Table/TableLayout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './TableLayout.css';
+
+function TableLayout() {
+  return (
+    <div className="container">
+      <div className='box1 box'>LOREM IPSUM</div>
+      <div className='box2 box'>LINK 1</div>
+      <div className='box3 box'>THIS IS SOME CONTENT ALIGNED IN THE CENTER VERTICALY AND HORIZONTALLY</div>
+      <div className='box4 box'></div>
+      <div className='box5 box'>THIS IS FOOTER SECTION WHICH IS ALIGNED LEFT</div>
+    </div>
+  );
+}
+
+export default TableLayout;
